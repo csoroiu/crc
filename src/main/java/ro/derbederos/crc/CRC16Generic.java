csoroiu/compression-benchmark

crc classes refactor

src/main/java/ro/derbederos/crc/CRC16Generic.java
@@ -6,23 +6,25 @@ import java.util.zip.Checksum;
  * http://en.wikipedia.org/wiki/Cyclic_redundancy_check
  * http://reveng.sourceforge.net/crc-catalogue/
  */
-public class CRC32 implements Checksum {
+public class CRC16Generic implements Checksum {
 
-    final private int lookupTable[] = new int[0x100];
-    final private int poly;
-    final private int initialValue;
+    final private short lookupTable[] = new short[0x100];
+    final private int width;
+    final private short poly;
+    final private short initialValue;
     final private boolean refIn; // reflect input data bytes
     final private boolean refOut; // resulted sum needs to be reversed before xor
-    final private int xorOut;
-    private int crc;
+    final private short xorOut;
+    private short crc;
 
-    public CRC32(int poly, int initialValue,
-                 boolean refIn, boolean refOut, int xorOut) {
-        this.poly = poly;
-        this.initialValue = initialValue;
+    public CRC16Generic(int width, int poly, int initialValue,
+                        boolean refIn, boolean refOut, int xorOut) {
+        this.width = width;
+        this.poly = (short) poly;
+        this.initialValue = (short) initialValue;
         this.refIn = refIn;
         this.refOut = refOut;
-        this.xorOut = xorOut;
+        this.xorOut = (short) xorOut;
         if (refIn) {
             initLookupTableReflected();
         } else {
@@ -32,14 +34,14 @@ public class CRC32 implements Checksum {
     }
 
     private void initLookupTableReflected() {
-        int poly = Integer.reverse(this.poly);
+        short poly = reverseShort(this.poly << 16 - width);
         for (int i = 0; i < 0x100; i++) {
-            int v = i;
+            short v = (short) i;
             for (int j = 0; j < 8; j++) {
                 if ((v & 1) == 1) {
-                    v = (v >>> 1) ^ poly;
+                    v = (short) (((v & 0xFFFF) >>> 1) ^ poly);
                 } else {
-                    v = v >>> 1;
+                    v = (short) ((v & 0xFFFF) >>> 1);
                 }
             }
             lookupTable[i] = v;
@@ -47,14 +49,14 @@ public class CRC32 implements Checksum {
     }
 
     private void initLookupTableUnreflected() {
-        int poly = this.poly;
+        short poly = (short) (this.poly << 16 - width);
         for (int i = 0; i < 0x100; i++) {
-            int v = i << 24;
+            short v = (short) (i << 8);
             for (int j = 0; j < 8; j++) {
                 if ((v & 0x8000000000000000L) != 0) {
-                    v = (v << 1) ^ poly;
+                    v = (short) ((v << 1) ^ poly);
                 } else {
-                    v = (v << 1);
+                    v = (short) (v << 1);
                 }
             }
             lookupTable[i] = v;
@@ -63,17 +65,17 @@ public class CRC32 implements Checksum {
 
     public void reset() {
         if (refIn) {
-            crc = Integer.reverse(initialValue);
+            crc = reverseShort(initialValue << 16 - width);
         } else {
-            crc = initialValue;
+            crc = (short) (initialValue << 16 - width);
         }
     }
 
     public void update(int b) {
         if (refIn) {
-            crc = (crc >>> 8) ^ lookupTable[(crc ^ b) & 0xff];
+            crc = (short) ((((int) crc & 0xFFFF) >>> 8) ^ lookupTable[((crc ^ b) & 0xff)]);
         } else {
-            crc = (crc << 8) ^ lookupTable[((crc >>> 24) ^ b) & 0xff];
+            crc = (short) ((((int) crc & 0xFFFF) << 8) ^ lookupTable[((crc >>> 8) ^ b) & 0xff]);
         }
     }
 
@@ -92,22 +94,37 @@ public class CRC32 implements Checksum {
     private void updateReflected(byte[] src, int offset, int len) {
         for (int i = offset; i < offset + len; i++) {
             int value = src[i];
-            crc = (crc >>> 8) ^ lookupTable[(crc ^ value) & 0xff];
+            crc = (short) ((((int) crc & 0xFFFF) >>> 8) ^ lookupTable[((crc ^ value) & 0xff)]);
         }
     }
 
     private void updateUnreflected(byte[] src, int offset, int len) {
         for (int i = offset; i < offset + len; i++) {
             int value = src[i];
-            crc = (crc << 8) ^ lookupTable[((crc >>> 24) ^ value) & 0xff];
+            crc = (short) ((((int) crc & 0xFFFF) << 8) ^ lookupTable[((crc >>> 8) ^ value) & 0xff]);
         }
     }
 
     public long getValue() {
-        if (refOut == refIn) {
-            return ((long) (crc ^ xorOut)) & 0xFFFFFFFFL;
-        } else {
-            return ((long) (Integer.reverse(crc) ^ xorOut)) & 0xFFFFFFFFL;
+        short xorOut = this.xorOut;
+        if (!refOut) {
+            xorOut <<= 16 - width;
         }
+
+        long result;
+        if (refOut == refIn) {
+            result = (crc ^ xorOut) & 0xFFFFL;
+        } else {
+            result = (reverseShort(crc) ^ xorOut) & 0xFFFFL;
+        }
+
+        if (!refOut) {
+            result >>>= 16 - width;
+        }
+        return result;
+    }
+
+    private static short reverseShort(int i) {
+        return (short) (Integer.reverse(i) >>> 16);
     }
 }

src/test/java/ro/derbederos/crc/CRC32GenericTest.java
@@ -9,27 +9,27 @@ import java.util.List;
 import java.util.zip.Checksum;
 
 import static org.junit.Assert.assertEquals;
-import static ro.derbederos.crc.Util.longToBytes;
+import static ro.derbederos.crc.Util.intToBytes;
 import static ro.derbederos.crc.Util.roundToByte;
 
 @RunWith(Parameterized.class)
-public class CRC64GenericTest {
+public class CRC32GenericTest {
     private static final byte[] testInput = "123456789".getBytes();
     private CRCParameters crcParameters;
 
-    public CRC64GenericTest(CRCParameters crcParameters) {
+    public CRC32GenericTest(CRCParameters crcParameters) {
         this.crcParameters = crcParameters;
     }
 
     @Test
     public void testCRCValue() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcParameters.getWidth(),
-                crcParameters.getPoly(),
-                crcParameters.getInitialValue(),
+                (int) crcParameters.getPoly(),
+                (int) crcParameters.getInitialValue(),
                 crcParameters.getRefIn(),
                 crcParameters.getRefOut(),
-                crcParameters.getXorOut());
+                (int) crcParameters.getXorOut());
         checksum.reset();
         checksum.update(testInput, 0, testInput.length);
         long value = checksum.getValue();
@@ -38,13 +38,13 @@ public class CRC64GenericTest {
 
     @Test
     public void testCRCValueUpdateOneByOne() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcParameters.getWidth(),
-                crcParameters.getPoly(),
-                crcParameters.getInitialValue(),
+                (int) crcParameters.getPoly(),
+                (int) crcParameters.getInitialValue(),
                 crcParameters.getRefIn(),
                 crcParameters.getRefOut(),
-                crcParameters.getXorOut());
+                (int) crcParameters.getXorOut());
         checksum.reset();
         for (byte inputByte : testInput) {
             checksum.update(inputByte);
@@ -55,42 +55,28 @@ public class CRC64GenericTest {
 
     @Test
     public void testResidue() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcParameters.getWidth(),
-                crcParameters.getPoly(),
+                (int) crcParameters.getPoly(),
                 0,
                 crcParameters.getRefIn(),
                 crcParameters.getRefOut(),
                 0);
 
-        long input = crcParameters.getXorOut();
+        int input = (int) crcParameters.getXorOut();
         if (crcParameters.getRefOut()) {
             //TODO: hack, fixes issue with CRC-5/USB
-            input = Long.reverse(input) >>> 64 - roundToByte(crcParameters.getWidth());
+            input = Integer.reverse(input) >>> 32 - roundToByte(crcParameters.getWidth());
         }
-        byte[] newByte = longToBytes(input);
+        byte[] newByte = intToBytes(input);
         int len = roundToByte(crcParameters.getWidth()) / 8;
-        checksum.update(newByte, 8 - len, len);
+        checksum.update(newByte, 4 - len, len);
         long residue = checksum.getValue();
         assertEquals(Long.toHexString(crcParameters.getResidue()), Long.toHexString(residue));
     }
 
     @Parameterized.Parameters(name = "{0}")
     public static List<CRCParameters> getCRCParameters() {
-        //CRC-64
-        CRCParameters crc64 = new CRCParameters("CRC-64", 64, 0x42F0E1EBA9EA3693L, 0,
-                false, false, 0, 0x6c40df5f0b497347L, 0);
-        CRCParameters crc64goiso = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
-                true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
-        CRCParameters crc64we = new CRCParameters("CRC-64/WE", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
-                false, false, 0xFFFFFFFFFFFFFFFFL, 0x62ec59e3f1a4f00aL, 0xfcacbebd5931a992L);
-        CRCParameters crc64xz = new CRCParameters("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
-                true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-
-        //CRC-40/GSM
-        CRCParameters crc40GSM = new CRCParameters("CRC-40/GSM", 40, 0x0004820009L, 0,
-                false, false, 0xFFFFFFFFFFL, 0xD4164FC646L, 0xC4FF8071FFL);
-
         //CRC-32
         CRCParameters crc32 = new CRCParameters("CRC-32", 32, 0x04C11DB7L, 0xFFFFFFFFL,
                 true, true, 0xFFFFFFFFL, 0xcbf43926L, 0xdebb20e3L);
@@ -323,8 +309,7 @@ public class CRC64GenericTest {
                 true, true, 0, 0x6, 0);
 
 
-        return Arrays.asList(crc64, crc64goiso, crc64we, crc64xz,
-                crc40GSM,
+        return Arrays.asList(
                 crc32, crc32autosar, crc32bzip2, crc32c, crc32d, crc32mpeg2, crc32posix, crc32q, jamcrc, xfer,
                 crc31philips,
                 crc30cdma,

csoroiu/crc

src/main/java/ro/derbederos/crc/CRC16Generic.java
@@ -2,6 +2,8 @@ package ro.derbederos.crc;
 
 import java.util.zip.Checksum;
 
+import static ro.derbederos.crc.Util.reverseShort;
+
 /*
  * http://en.wikipedia.org/wiki/Cyclic_redundancy_check
  * http://reveng.sourceforge.net/crc-catalogue/
@@ -9,26 +11,26 @@ import java.util.zip.Checksum;
  */
 
 /**
- * Byte-wise CRC implementation that can compute CRC with width <= 64 using different models.
+ * Byte-wise CRC implementation that can compute CRC with width <= 16 using different models.
  */
-public class CRC64Generic implements Checksum {
+public class CRC16Generic implements Checksum {
 
-    final private long lookupTable[] = new long[0x100];
+    final private short lookupTable[] = new short[0x100];
     final private int width;
-    final private long poly;
-    final private long init;
+    final private short poly;
+    final private short init;
     final private boolean refIn; // reflect input data bytes
     final private boolean refOut; // resulted sum needs to be reversed before xor
-    final private long xorOut;
-    private long crc;
+    final private short xorOut;
+    private short crc;
 
-    public CRC64Generic(int width, long poly, long init, boolean refIn, boolean refOut, long xorOut) {
+    public CRC16Generic(int width, int poly, int init, boolean refIn, boolean refOut, int xorOut) {
         this.width = width;
-        this.poly = poly;
-        this.init = init;
+        this.poly = (short) poly;
+        this.init = (short) init;
         this.refIn = refIn;
         this.refOut = refOut;
-        this.xorOut = xorOut;
+        this.xorOut = (short) xorOut;
         if (refIn) {
             initLookupTableReflected();
         } else {
@@ -38,14 +40,14 @@ public class CRC64Generic implements Checksum {
     }
 
     private void initLookupTableReflected() {
-        long poly = Long.reverse(this.poly << 64 - width);
+        short poly = reverseShort(this.poly << 16 - width);
         for (int i = 0; i < 0x100; i++) {
-            long v = i;
+            short v = (short) i;
             for (int j = 0; j < 8; j++) {
                 if ((v & 1) == 1) {
-                    v = (v >>> 1) ^ poly;
+                    v = (short) (((v & 0xFFFF) >>> 1) ^ poly);
                 } else {
-                    v = (v >>> 1);
+                    v = (short) ((v & 0xFFFF) >>> 1);
                 }
             }
             lookupTable[i] = v;
@@ -53,14 +55,14 @@ public class CRC64Generic implements Checksum {
     }
 
     private void initLookupTableUnreflected() {
-        long poly = this.poly << 64 - width;
+        short poly = (short) (this.poly << 16 - width);
         for (int i = 0; i < 0x100; i++) {
-            long v = ((long) i) << 56;
+            short v = (short) (i << 8);
             for (int j = 0; j < 8; j++) {
                 if ((v & 0x8000000000000000L) != 0) {
-                    v = (v << 1) ^ poly;
+                    v = (short) ((v << 1) ^ poly);
                 } else {
-                    v = (v << 1);
+                    v = (short) (v << 1);
                 }
             }
             lookupTable[i] = v;
@@ -69,17 +71,17 @@ public class CRC64Generic implements Checksum {
 
     public void reset() {
         if (refIn) {
-            crc = Long.reverse(init << 64 - width);
+            crc = reverseShort(init << 16 - width);
         } else {
-            crc = init << 64 - width;
+            crc = (short) (init << 16 - width);
         }
     }
 
     public void update(int b) {
         if (refIn) {
-            crc = (crc >>> 8) ^ lookupTable[(int) ((crc ^ b) & 0xff)];
+            crc = (short) ((((int) crc & 0xFFFF) >>> 8) ^ lookupTable[((crc ^ b) & 0xff)]);
         } else {
-            crc = (crc << 8) ^ lookupTable[(int) (((crc >>> 56) ^ b) & 0xff)];
+            crc = (short) ((((int) crc & 0xFFFF) << 8) ^ lookupTable[((crc >>> 8) ^ b) & 0xff]);
         }
     }
 
@@ -98,32 +100,32 @@ public class CRC64Generic implements Checksum {
     private void updateReflected(byte[] src, int offset, int len) {
         for (int i = offset; i < offset + len; i++) {
             int value = src[i];
-            crc = (crc >>> 8) ^ lookupTable[(int) ((crc ^ value) & 0xff)];
+            crc = (short) ((((int) crc & 0xFFFF) >>> 8) ^ lookupTable[((crc ^ value) & 0xff)]);
         }
     }
 
     private void updateUnreflected(byte[] src, int offset, int len) {
         for (int i = offset; i < offset + len; i++) {
             int value = src[i];
-            crc = (crc << 8) ^ lookupTable[(int) (((crc >>> 56) ^ value) & 0xff)];
+            crc = (short) ((((int) crc & 0xFFFF) << 8) ^ lookupTable[((crc >>> 8) ^ value) & 0xff]);
         }
     }
 
     public long getValue() {
-        long xorOut = this.xorOut;
+        short xorOut = this.xorOut;
         if (!refOut) {
-            xorOut <<= 64 - width;
+            xorOut <<= 16 - width;
         }
 
         long result;
         if (refOut == refIn) {
-            result = crc ^ xorOut;
+            result = (crc ^ xorOut) & 0xFFFFL;
         } else {
-            result = Long.reverse(crc) ^ xorOut;
+            result = (reverseShort(crc) ^ xorOut) & 0xFFFFL;
         }
 
         if (!refOut) {
-            result >>>= 64 - width;
+            result >>>= 16 - width;
         }
         return result;
     }

src/test/java/ro/derbederos/crc/CRC32GenericTest.java
@@ -9,28 +9,27 @@ import java.util.List;
 import java.util.zip.Checksum;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static ro.derbederos.crc.Util.longToBytes;
+import static ro.derbederos.crc.Util.intToBytes;
 import static ro.derbederos.crc.Util.roundToByte;
 
 @RunWith(Parameterized.class)
-public class CRC64GenericTest {
+public class CRC32GenericTest {
     private static final byte[] testInput = "123456789".getBytes();
     private CRCModel crcModel;
 
-    public CRC64GenericTest(CRCModel crcModel) {
+    public CRC32GenericTest(CRCModel crcModel) {
         this.crcModel = crcModel;
     }
 
     @Test
     public void testCRCValue() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcModel.getWidth(),
-                crcModel.getPoly(),
-                crcModel.getInit(),
+                (int) crcModel.getPoly(),
+                (int) crcModel.getInit(),
                 crcModel.getRefIn(),
                 crcModel.getRefOut(),
-                crcModel.getXorOut());
+                (int) crcModel.getXorOut());
         checksum.reset();
         checksum.update(testInput, 0, testInput.length);
         long value = checksum.getValue();
@@ -39,13 +38,13 @@ public class CRC64GenericTest {
 
     @Test
     public void testCRCValueUpdateOneByOne() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcModel.getWidth(),
-                crcModel.getPoly(),
-                crcModel.getInit(),
+                (int) crcModel.getPoly(),
+                (int) crcModel.getInit(),
                 crcModel.getRefIn(),
                 crcModel.getRefOut(),
-                crcModel.getXorOut());
+                (int) crcModel.getXorOut());
         checksum.reset();
         for (byte inputByte : testInput) {
             checksum.update(inputByte);
@@ -56,52 +55,28 @@ public class CRC64GenericTest {
 
     @Test
     public void testResidue() {
-        Checksum checksum = new CRC64Generic(
+        Checksum checksum = new CRC32Generic(
                 crcModel.getWidth(),
-                crcModel.getPoly(),
+                (int) crcModel.getPoly(),
                 0,
                 crcModel.getRefIn(),
                 crcModel.getRefOut(),
                 0);
 
-        long input = crcModel.getXorOut();
+        int input = (int) crcModel.getXorOut();
         if (crcModel.getRefOut()) {
             //TODO: hack, fixes issue with CRC-5/USB
-            input = Long.reverse(input) >>> 64 - roundToByte(crcModel.getWidth());
+            input = Integer.reverse(input) >>> 32 - roundToByte(crcModel.getWidth());
         }
-        byte[] newByte = longToBytes(input);
+        byte[] newByte = intToBytes(input);
         int len = roundToByte(crcModel.getWidth()) / 8;
-        checksum.update(newByte, 8 - len, len);
+        checksum.update(newByte, 4 - len, len);
         long residue = checksum.getValue();
         assertEquals(Long.toHexString(crcModel.getResidue()), Long.toHexString(residue));
     }
 
-    @Test
-    public void testCRCValueSelfTest() {
-        assertTrue(CRCModelSelfTest.validateCRCValue(crcModel));
-    }
-
-    @Test
-    public void testResidueSelfTest() {
-        assertTrue(CRCModelSelfTest.validateCRCResidue(crcModel));
-    }
-
     @Parameterized.Parameters(name = "{0}")
     public static List<CRCModel> getCRCParameters() {
-        //CRC-64
-        CRCModel crc64 = new CRCModel("CRC-64", 64, 0x42F0E1EBA9EA3693L, 0,
-                false, false, 0, 0x6c40df5f0b497347L, 0);
-        CRCModel crc64goiso = new CRCModel("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
-                true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
-        CRCModel crc64we = new CRCModel("CRC-64/WE", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
-                false, false, 0xFFFFFFFFFFFFFFFFL, 0x62ec59e3f1a4f00aL, 0xfcacbebd5931a992L);
-        CRCModel crc64xz = new CRCModel("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
-                true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-
-        //CRC-40/GSM
-        CRCModel crc40GSM = new CRCModel("CRC-40/GSM", 40, 0x0004820009L, 0,
-                false, false, 0xFFFFFFFFFFL, 0xD4164FC646L, 0xC4FF8071FFL);
-
         //CRC-32
         CRCModel crc32 = new CRCModel("CRC-32", 32, 0x04C11DB7L, 0xFFFFFFFFL,
                 true, true, 0xFFFFFFFFL, 0xcbf43926L, 0xdebb20e3L);
@@ -334,8 +309,7 @@ public class CRC64GenericTest {
                 true, true, 0, 0x6, 0);
 
 
-        return Arrays.asList(crc64, crc64goiso, crc64we, crc64xz,
-                crc40GSM,
+        return Arrays.asList(
                 crc32, crc32autosar, crc32bzip2, crc32c, crc32d, crc32mpeg2, crc32posix, crc32q, jamcrc, xfer,
                 crc31philips,
                 crc30cdma,