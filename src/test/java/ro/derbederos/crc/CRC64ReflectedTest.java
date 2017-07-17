csoroiu/compression-benchmark

added crc classes (residual value check)

src/test/java/ro/derbederos/crc/CRC64ReflectedTest.java
@@ -34,10 +34,10 @@ public class CRC64ReflectedTest {
 
     @Parameterized.Parameters
     public static List<CRCParameters> getCRCParameters() {
-        CRCParameters crc64go = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64iso = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
-        CRCParameters crc64xz = new CRCParameters("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64ecma = new CRCParameters("CRC-64/XZ-ECMA", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-        return Arrays.asList(crc64go, crc64xz);
+        return Arrays.asList(crc64iso, crc64ecma);
     }
 }

src/test/java/ro/derbederos/crc/CRC64Test.java
@@ -37,12 +37,12 @@ public class CRC64Test {
     public static List<CRCParameters> getCRCParameters() {
         CRCParameters crc64 = new CRCParameters("CRC-64", 64, 0x42F0E1EBA9EA3693L, 0,
                 false, false, 0, 0x6c40df5f0b497347L, 0);
-        CRCParameters crc64go = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64iso = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
         CRCParameters crc64we = new CRCParameters("CRC-64/WE", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 false, false, 0xFFFFFFFFFFFFFFFFL, 0x62ec59e3f1a4f00aL, 0xfcacbebd5931a992L);
-        CRCParameters crc64xz = new CRCParameters("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64ecma = new CRCParameters("CRC-64/XZ-ECMA", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-        return Arrays.asList(crc64, crc64go, crc64we, crc64xz);
+        return Arrays.asList(crc64, crc64iso, crc64we, crc64ecma);
     }
 }

csoroiu/crc

src/test/java/ro/derbederos/crc/CRC64ReflectedTest.java
@@ -32,12 +32,12 @@ public class CRC64ReflectedTest {
         assertEquals(Long.toHexString(crcParameters.getCheck()), Long.toHexString(value));
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "{0}")
     public static List<CRCParameters> getCRCParameters() {
-        CRCParameters crc64go = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64goiso = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
         CRCParameters crc64xz = new CRCParameters("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-        return Arrays.asList(crc64go, crc64xz);
+        return Arrays.asList(crc64goiso, crc64xz);
     }
 }

src/test/java/ro/derbederos/crc/CRC64Test.java
@@ -33,16 +33,16 @@ public class CRC64Test {
         assertEquals(Long.toHexString(crcParameters.getCheck()), Long.toHexString(value));
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "{0}")
     public static List<CRCParameters> getCRCParameters() {
         CRCParameters crc64 = new CRCParameters("CRC-64", 64, 0x42F0E1EBA9EA3693L, 0,
                 false, false, 0, 0x6c40df5f0b497347L, 0);
-        CRCParameters crc64go = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
+        CRCParameters crc64goiso = new CRCParameters("CRC-64/GO-ISO", 64, 0x000000000000001bL, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0xb90956c775a41001L, 0x5300000000000000L);
         CRCParameters crc64we = new CRCParameters("CRC-64/WE", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 false, false, 0xFFFFFFFFFFFFFFFFL, 0x62ec59e3f1a4f00aL, 0xfcacbebd5931a992L);
         CRCParameters crc64xz = new CRCParameters("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693L, 0xFFFFFFFFFFFFFFFFL,
                 true, true, 0xFFFFFFFFFFFFFFFFL, 0x995dc9bbdf1939faL, 0x49958c9abd7d353fL);
-        return Arrays.asList(crc64, crc64go, crc64we, crc64xz);
+        return Arrays.asList(crc64, crc64goiso, crc64we, crc64xz);
     }
 }